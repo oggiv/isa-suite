050021bb9358e496acc9f851836f55a610a14448
Viggo <93593542+oggiv@users.noreply.github.com>
2023-12-07 22:37:25 +0100

    Added non-funct instructions and basic debug

--- emulate.c
+++ emulate.c
@@ -24,11 +24,70 @@
 
 int main(int argc, char const *argv[])
 {
-  unsigned char inputByte = 0b01000000;
 
-  unsigned int opCode = (inputByte & 0b11000000) >> 6;
+  // manually inputing binaries for testing
+  unsigned char testProgram[] = {
+    0b11000110, // put 6 in a
+    0b00001000, // copy reg[a] to reg[1]
+    0b00010001  // copy reg[1] to reg[2]
+  };
 
-  printf("%d\n", opCode);
+  // every position in reg is a CPU register
+  signed char reg[8];
+
+  // PC keeps track of which instruction the program is on
+  unsigned int programCounter;
+
+  // run the program
+  unsigned char inputByte;
+  unsigned int opCode;
+  for (programCounter = 0; programCounter < sizeof(testProgram); programCounter++) {
+
+    inputByte = testProgram[programCounter];
+    unsigned int opCode = (inputByte & 0b11000000) >> 6;
+
+    int imm;
+    int arg1 = (inputByte & 0b00111000) >> 3;
+    int arg2 = (inputByte & 0b00000111);
+
+    switch (opCode) {
+      case 0b00: // move
+        // copy value from the arg2 register to the arg1 register
+        reg[arg1] = reg[arg2];
+        break;
+
+      case 0b01: // bounce if not zero
+        // jump to the address in the first register if the value in the second register is not zero
+        if (reg[arg2] != 0) {
+          programCounter = reg[arg1];
+        }
+        break;
+
+      case 0b10: // functional operations
+        // code
+        break;
+
+      case 0b11: // write immediate
+        // code
+        imm = arg1 | arg2;
+        reg[0] = imm;
+        break;
+
+      default: // unknown opcode
+        break;
+    }
+  }
+
+  printf("PC: %d\n", programCounter);
+  printf("Registers\n");
+  printf(" $a %d\n", reg[0]);
+  printf(" $1 %d\n", reg[1]);
+  printf(" $2 %d\n", reg[2]);
+  printf(" $3 %d\n", reg[3]);
+  printf(" $4 %d\n", reg[4]);
+  printf(" $5 %d\n", reg[5]);
+  printf(" $6 %d\n", reg[6]);
+  printf(" $7 %d\n", reg[7]);
 
   return 0;
 }
